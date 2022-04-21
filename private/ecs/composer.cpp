5d6a7860db7604a921c7282dc20ba8ceea0b4614
István Schoffhauzer <schoffi92@gmail.com>
2022-04-21 19:19:45 +0200

    DeltaSeconds added to system

--- private/ecs/composer.cpp
+++ private/ecs/composer.cpp
@@ -6,13 +6,14 @@ void ztech::ecs::composer::run( )
     std::shared_lock< std::shared_mutex > lock( composer_links_mutex );
     std::vector< std::unique_ptr< std::thread > > threads( composer_links.size( ) );
     int thread_index = 0;
+    float deltaSeconds = get_delta_seconds( );
     for ( auto it = std::begin( composer_links ); it != std::end( composer_links ); it++ )
     {
         threads[ thread_index ] = std::make_unique< std::thread >( [&]( const composer_link_t& link )
         {
             for ( auto sys : link.systems )
             {
-                sys->execute( link.entities );
+                sys->execute( link.entities, deltaSeconds );
             }
         }, it->second);
         thread_index++;
